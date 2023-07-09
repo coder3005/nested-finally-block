be5548a8caec2823001b8f335baa87c5a26abe13
ABHI GUPTA <138576028+coder3005@users.noreply.github.com>
2023-07-09 15:19:56 +0530

    Create nestedfinally1.java

---
+++ nestedfinally1.java
@@ -0,0 +1,32 @@
+/* try-catch-finally inside finally block */
+class nestedfinally
+{
+    public static void main(String[] args) {
+        
+        try
+        {
+            String a="abhi";
+            System.out.println(a.toUpperCase());
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        finally
+        {
+            try
+            {
+                System.out.println(10/2);
+            }
+            catch(ArithmeticException a)
+            {
+                System.out.println(a);
+            }
+            finally
+            {
+                System.out.println("Learn Coding");
+            }
+        }
+        System.out.println("main method ended");
+    }
+}
